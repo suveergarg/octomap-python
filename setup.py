9ec68e2c97c2bf3ac2402fac1473c6f404d6ca7f
gsuveer <suveer.garg@samsung.com>
2022-08-16 15:08:36 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -10,8 +10,11 @@ def get_long_description():
     return long_description
 
 
-subprocess.run(['pip', "install", "numpy"], check=True)
-subprocess.run(['pip', "install", "cython"], check=True)
+#subprocess.run(['pip', "install", "numpy"], check=True)
+#subprocess.run(['pip', "install", "cython"], check=True)
+
+import pip
+pip.main(['install', 'numpy', 'cython']) 
 
 from Cython.Distutils import build_ext
 import numpy
